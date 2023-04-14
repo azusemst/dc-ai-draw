2562b798bbae0113fa49ad38ef3b8dba018c2a0d
Chengzhi <zmq175@qq.com>
2023-04-15 01:16:16 +0800

    redis

--- commands/draw.js
+++ commands/draw.js
@@ -78,11 +78,11 @@ module.exports = {
                 height: height
             })
         };
+        const uid = new ShortUniqueId();
+        const uuid = uid();
         keyv.set(uuid, JSON.stringify(request));
         const response = await fetch('http://121.41.44.246:8080/sdapi/v1/txt2img', request);
         const data = await response.json();
-        const uid = new ShortUniqueId();
-        const uuid = uid();
 
         const generateNewBtn = new ButtonBuilder()
             .setCustomId(`generateNew-${uuid}`)    
